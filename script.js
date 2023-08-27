a94c02ca65d75434b62466f6938802d8653419e5
Saatvik Yadav <76916280+SaatvikYadav12@users.noreply.github.com>
2023-08-27 23:56:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ console.log(document.querySelector('.guess').value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
